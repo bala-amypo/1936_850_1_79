f19e325f57eacf4f2c0531db6c4e818b88559d9c
bala-amypo <bala@amypo.in>
2025-12-22 09:45:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+    public String test() {
+        return "Location Service Working";
+    }
+}
---
+++ src/main/java/com/example/demo/service/ShipmentService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShipmentService {
+
+    public String test() {
+        return "Shipment Service Working";
+    }
+}
